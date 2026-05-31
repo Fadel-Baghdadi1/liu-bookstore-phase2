a7c9288129934c346a461a8abbbd37a7eed6a682
Fadel Baghdadi <72430620@students.liu.edu.lb>
2026-05-31 20:34:03 +0300

    Adding Documentation to make the project clear and readable

--- fadelbaghdadi-ahmadkondakji-webproject/src/index.js
+++ fadelbaghdadi-ahmadkondakji-webproject/src/index.js
@@ -1,12 +1,24 @@
+/**
+ * Entry Point: index.js
+ * This file is responsible for "mounting" the React application into the 
+ * physical DOM of the web browser.
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// If you have a global CSS file, keep this line. 
-// Otherwise, Bootstrap from your index.html handles the rest.
-import './index.css'; 
+import './index.css'; // Importing global styles
 
+// Locate the 'root' div in the public/index.html file.
+// This is where the entire React application will be "injected".
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the application
 root.render(
+  /**
+   * React.StrictMode is a wrapper that helps find potential problems 
+   * in the application during development. It doesn't render any visible UI.
+   */
   <React.StrictMode>
     <App />
   </React.StrictMode>
